5d43477660a4de97ba8d46acab078e4648232846
zubiate <52678814+zubiate@users.noreply.github.com>
2019-12-12 14:54:16 -0600

    Create ExhibitingOrganisationsMultilingualTrait.php
    Create ExhibitingOrganisationsMultilingual Trait

---
+++ src/Traits/Query/ExhibitingOrganisationsMultilingualTrait.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Gtechmx\Atlas\Traits\Query;
+
+trait ExhibitingOrganisationsMultilingualTrait
+{
+    protected  function geExhibitingOrganisationsMultilingual($query)
+    {
+        if (!$query) {
+            return self::fullExhibitingOrganisationsMultilingualQuery();
+        }
+        return $query;
+    }
+
+    private  function fullExhibitingOrganisationsMultilingualQuery()
+    {
+        return '{
+  exhibitingOrganisationsMultilingual(
+    eventEditionId: "'.$this->event_id.'"
+  ){
+      companyName,
+      multilingual {
+          addressLine1,
+          displayName,
+          showObjective,
+          description,
+          locale
+
+      },
+      socialMedia {
+          name,
+          url
+      },
+      stands {
+          name
+      },
+      productsAndServicesMapping  {
+          id,
+          multilingualItems
+          {
+              locale,
+              name
+          }
+      },
+      website,
+      contactEmail,
+      phone,
+      exhibitorStatus,
+      organisationId,
+      id
+  }
+}';
+    }
+}
