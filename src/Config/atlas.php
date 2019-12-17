2d43f030e6f00f01014a9a67aae838fbadec33eb
zubiate <52678814+zubiate@users.noreply.github.com>
2019-12-17 11:59:55 -0600

    Update atlas.php
    Change url sandbox for enviroment

--- src/Config/atlas.php
+++ src/Config/atlas.php
@@ -1,14 +1,11 @@
 <?php
 
 return [
-
- 
     'sanbox' => env('ATLAS_SANBOX', false),
     'api_key' =>  env('ATLAS_SANBOX') ? env('SANBOX_ATLAS_XCLIENTID', 'default'): env('ATLAS_XCLIENTID', 'default'),
     'client_id' =>  env('ATLAS_SANBOX') ? env('SANBOX_ATLAS_CLIENT_ID', 'default') : env('ATLAS_CLIENT_ID', 'default'),
     'client_secret' => env('ATLAS_SANBOX') ? env('SANBOX_ATLAS_SECRET', 'default') :  env('ATLAS_SECRET', 'default'),
-    'grant_type' =>  env('ATLAS_SANBOX') ? env('SANBOX_ATLAS_GRANT_TYPE', 'client_credentials') : env('ATLAS_GRANT_TYPE', 'client_credentials'),
+    'grant_type' =>  env('ATLAS_SANBOX') ? env('SANBOX_ATLAS_GRANT_TYPE', '') : env('ATLAS_GRANT_TYPE', ''),
     'scopes' =>  env('ATLAS_SANBOX') ? env('SANBOX_ATLAS_SCOPES', '') : env('ATLAS_SCOPES', ''),
-    'url' => env('ATLAS_SANBOX') ? 'https://api.rxweb-pre.com' : 'https://api.reedexpo.com'
-
+    'url' => env('ATLAS_SANBOX') ? env('ATLAS_URL_SANBOX', '') : env('ATLAS_URL', '')
 ];
